bf9e7cad8d2daed2a620717eec5c4b48f1b75032
Miguel Salinas <eliassid2@gmail.com>
2023-07-13 11:01:23 -0300

    se agrega ruta para agregar comentarios

--- src/services/commentservice.php
+++ src/services/commentservice.php
@@ -19,3 +19,51 @@ function getComments($id)
     $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $posts;
 }
+
+
+// Agrega un nuevo comentario
+function commentAdd($request, $response)
+{
+    global $pdo;
+    $data = [];
+    try {
+        $body = $request->getBody();
+        $jsonData = json_decode($body, true);
+        $requiredFields = ['user_id', 'post_id', 'contenido'];
+        foreach ($requiredFields as $field) {
+            if (!isset($jsonData[$field]) || empty($jsonData[$field])) {
+                $data = ['success' => false, 'message' => 'Faltan campos requeridos o están vacíos.'];
+                return $data;
+            }
+        }
+        $query = "INSERT INTO comments (user_id, post_id, contenido) VALUES (?,?,?)";
+        $statement = $pdo->prepare($query);
+        $values = [
+            $jsonData['user_id'],
+            $jsonData['post_id'],
+            $jsonData['contenido']
+        ];
+        $statement->execute($values);
+        $commentId = $pdo->lastInsertId();
+        if ($commentId) {
+            $comment = getOneComment($commentId);
+            $data = ['success' => true, 'data' => $comment];
+        }
+    } catch (Exception $e) {
+        $data = ['success' => false, 'message' => 'Error: ' . $e->getMessage()];
+    }
+    return $data;
+}
+
+
+// Trae un unico comentario - utilizado en agregar
+function getOneComment($id)
+{
+    global $pdo;
+    $query = 'SELECT * FROM comments c WHERE c.comment_id = ?';
+    $statement = $pdo->prepare($query);
+    $statement->bindParam(1, $id);
+    $statement->execute();
+    $comment = $statement->fetch(PDO::FETCH_ASSOC);
+    return $comment;
+}
